1fb6703c85fc985528a8e1d32c124bf2d527731e
Dirk Sliwka <49401450+dsliwka@users.noreply.github.com>
2025-09-25 19:36:54 +0200

    Update import_stata

--- tabout/__init__.py
+++ tabout/__init__.py
@@ -3,5 +3,6 @@ from .dtable import DTable
 from .etable import ETable
 from .btable import BTable
 from .extractors import register_extractor, clear_extractors, ModelExtractor
+from .importstata import import_stata
 
-__all__ = ["MTable","BTable", "DTable", "ETable", "register_extractor", "clear_extractors", "ModelExtractor"]
+__all__ = ["MTable","BTable", "DTable", "ETable", "register_extractor", "clear_extractors", "ModelExtractor", "import_stata"]
