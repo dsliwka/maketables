63b59ceb0cce6a16309d8d709f753dc50ff3d797
Dirk Sliwka <49401450+dsliwka@users.noreply.github.com>
2025-09-25 18:03:43 +0200

    add btable for balance tables

--- tabout/__init__.py
+++ tabout/__init__.py
@@ -1,6 +1,7 @@
 from .mtable import MTable
 from .dtable import DTable
 from .etable import ETable
+from .btable import BTable
 from .extractors import register_extractor, clear_extractors, ModelExtractor
 
-__all__ = ["BTable", "DTable", "ETable", "register_extractor", "clear_extractors", "ModelExtractor"]
+__all__ = ["MTable","BTable", "DTable", "ETable", "register_extractor", "clear_extractors", "ModelExtractor"]
